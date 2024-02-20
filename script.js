c595e06aaf14c0bc8a27dab3a99139a92acfa11e
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-02-20 18:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 class Rectangle {
-    constructor(width,height){
-        this._width=width;
-        this._height=height
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
     }
-    getArea(){
-        return this._height + this._width
+
+    getArea() {
+        return this._height * this._width; // Corrected calculation
     }
-    get width(){
-     return this._width
+
+    get width() {
+        return this._width;
     }
-    set height(height){
-        this._height = height
+
+    get height() { // Added getter for height
+        return this._height;
+    }
+
+    set height(height) {
+        this._height = height;
     }
 }
 
-class Square extends Rectangle{
-    constructor(side){
-        super(side,side)
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
     }
-    getPerimeter(){
-        return this._width*4
+
+    getPerimeter() {
+        return this._width * 4;
     }
 }
-const total = new Square(12,10)
-console.log(total.getPerimeter())
+
+const total = new Square(12, 10); // Corrected instantiation
+console.log(total.getPerimeter());
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
