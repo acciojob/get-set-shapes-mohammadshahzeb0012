d1e3e749cb622d04e19c2ca961f7283babc66025
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-02-20 18:41:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width,height){
+        this._width=width;
+        this._height=height
+    }
+    getArea(){
+        return this._height + this._width
+    }
+    get width(){
+     return this._width
+    }
+    set height(height){
+        this._height = height
+    }
+}
 
-class Square extends Animal {}
+class Square extends Rectangle{
+    constructor(side){
+        super(side,side)
+    }
+    getPerimeter(){
+        return this._width*4
+    }
+}
+const total = new Square(12,10)
+console.log(total.getPerimeter())
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
